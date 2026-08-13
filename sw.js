0e5783ab3afcde0896ef62a1b72d6a9efca59ca8
paddleandpinllc-lang <paddleandpinllc@gmail.com>
2026-08-13 13:53:40 -0400

    Release member hosting experience

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const APP_VERSION = "2026-08-13-pickle-date-2";
+const APP_VERSION = "2026-08-13-member-host-1";
 const CACHE_NAME = `club-society-${APP_VERSION}`;
 const versioned = (path) => `${path}?v=${encodeURIComponent(APP_VERSION)}`;
 const ASSETS = [
